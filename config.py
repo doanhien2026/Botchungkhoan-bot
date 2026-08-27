ed0fcd51d12c5774bace744fd36b7777e4072a78
doanhien2026 <doanthehien2023@gmail.com>
2026-08-28 04:23:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+# ====================== 🔧 CHỈ SỬA Ở ĐÂY ======================
 TELEGRAM_TOKEN = "8901722608:AAHnHfYsR8ilnHCHRaDUedA1ra1p0gPWda8"
 CHAT_ID = "-1001030583610"
 PORT = 10000
+DATA_FILE = "xsmb_data.json"
+AUTO_SEND_TIME = "18:35"
+# ==============================================================
